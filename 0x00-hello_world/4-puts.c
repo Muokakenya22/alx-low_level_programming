19ffb8537fe0af07b4bcc239426df1440853ed3e
marie <101573237+Muokakenya22@users.noreply.github.com>
2022-04-08 02:11:43 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-// C program to show how to use puts
+/*C program to show how to use puts*/
 #include <stdio.h>
 int main()
 {
 	puts("Programming is like building a multilingual puzzle");
 
 	getchar();
-	return (0);
+	return 0;
 }
