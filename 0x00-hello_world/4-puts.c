0f63cb5bf6a88db6f502a60c15a9b2bd7533d8a1
unknown <Muokakenya22>
2022-04-07 23:58:22 +0300

    c5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-int main () {
-	char str1[1];
-
-	strcpy(str1,""Programming is like building a multilingual puzzle");
-
-	puts(str1);
-
-	return(0);
+int main()
+{
+	char string["Programming is like building a multilingual puzzle];
+	strcpy(str,"This is q4");
+	puts(string);
+	return 0;
 }
