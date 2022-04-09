fbfcac4023cdf212b1f092b57076ceb71ca41f76
unknown <Muokakenya22>
2022-04-09 03:40:33 +0300

    learning c q04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 #include<stdio.h>
-=======
 #include <stdio.h>
 int main()
-
-	puts("Programming is like building a multilingual puzzle");
->>>>>>> 0df7b0b14ca678ddc22339b7e08bf62fbc19ea48
-
 /**
  * main - entry point
  *
