6651c9a8bb092e1db79e577ad7ccaf31cdd30b24
unknown <Muokakenya22>
2022-04-14 00:42:11 +0300

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* printf_alphabet - prints alphabets in lowercase
+*
+*
+*Return : 0
+*/
+
+void print_alphabet(void)
+{ 	
+	for(int i =97 ; i < 123;i++)
+	{
+		_putchar(i)
+	}
+	_putchar("\n");
+}
