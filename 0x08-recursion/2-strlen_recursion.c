05c9da4f054b48a0b82ad196e041e87f39d9d68d
marie <101573237+Muokakenya22@users.noreply.github.com>
2022-04-29 00:57:33 +0300

    Update 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - finds string length
+ * @s: the string
+ * Return: the length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
+© 2022 GitHub, Inc.
