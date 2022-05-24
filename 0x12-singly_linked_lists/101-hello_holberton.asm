6a69ec043a0b95ed3df5c356cd628aa51739d0d0
unknown <Muokakenya22>
2022-05-25 00:20:15 +0300

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+; Filename: hello_holberton.asm
+; Author: Adam Keino
+
+global 	main
+extern 	printf
+
+section .text
+
+main:
+	mov	rdi, format
+	mov	rax, 0
+	call	printf
+	mov	rax, 0
+	ret
+format:
+	db "Hello, Holberton", 10, 0
